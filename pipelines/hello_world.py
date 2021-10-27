aa0bfc1ec064040d93a6ef6b98d41043a30c652a
jpatacz <julia.patacz@skillcorner.com>
2021-10-27 11:17:56 +0200

    Initial commit

---
+++ pipelines/hello_world.py
@@ -0,0 +1,13 @@
+class HelloWorld:
+    def hello_world(self):
+        """This is hello world function docstring with new words"""
+
+        return "Hello World"
+
+    def second_hello_world(self, a=None, b=None):
+        """second_hello_world(a=None, b=None) -> None
+
+        Here I continue documentation
+        """
+
+        print("Second hello world")
