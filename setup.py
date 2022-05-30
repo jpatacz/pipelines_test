b29602d0c4eb36b0c9961a8c66821b0691904c76
jpatacz <julia.patacz@skillcorner.com>
2022-05-30 17:35:07 +0200

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pipelines_test',
     version='0.3.11',
     description='Test Pipelines',
-    author='Julia Patacz
+    author='Julia Patacz',
     author_email='julia.patacz.com',
     packages=['pipelines'],
     python_requires='>=3.6',
