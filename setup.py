05bb554103a9843a638ad43d5776bce3323bd3cb
jpatacz <julia.patacz@skillcorner.com>
2022-05-30 17:21:50 +0200

    Test

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from setuptools import setup
 
 setup(
     name='pipelines_test',
-    version='0.3.6',
-    description='Test Pipelines',
-    author='Julia Patacz',
-    author_email='julia.patacz@skillcorner.com',
+    version='1.0.4',
+    description='SkillCorner API client',
+    author='SkillCorner',
+    author_email='support@skillcorner.com',
     packages=['pipelines'],
-    python_requires='>=3.6, <=3.9',
+    python_requires='>=3.6',
     install_requires=[
-        'Sphinx>=4.0.0',
-        'sphinx-rtd-theme==1.0.0',
+        'requests>=2.20.0',
+        'makefun>=1.10.0'
     ],
+    extras_require={
+        'test': 'mock==4.0.3',
+        'release': ['Sphinx==4.2.0', 'sphinx-rtd-theme==1.0.0']
+    }
 )
